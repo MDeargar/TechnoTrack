fae00f8ff9c028f752864b6dbb92bf664e2d5658
MDeargar <mdeargar@gmail.com>
2020-10-12 00:32:33 +0300

    Clear master branch

--- homework-1/build/src/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mordekaj/Documents/TehnoTrack/homework-1/src
-# Build directory: /home/mordekaj/Documents/TehnoTrack/homework-1/build/src
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- homework-1/build/test/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mordekaj/Documents/TehnoTrack/homework-1/test
-# Build directory: /home/mordekaj/Documents/TehnoTrack/homework-1/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
