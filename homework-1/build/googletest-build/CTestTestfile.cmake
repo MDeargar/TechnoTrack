1c2e481cca2617014dbc0f12023a2faa53009327
MDeargar <mdeargar@gmail.com>
2020-10-12 00:26:05 +0300

    Remove files from another branch

--- homework-1/build/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mordekaj/Documents/TehnoTrack/homework-1/build/googletest-src
-# Build directory: /home/mordekaj/Documents/TehnoTrack/homework-1/build/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
