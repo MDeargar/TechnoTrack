fe0d4110f159d0fd1e5b45dc63dd830b2c28a1b6
MDeargar <mdeargar@gmail.com>
2020-10-12 01:00:59 +0300

    testing .gitignore

---
+++ homework-2/test/test_solver.cpp
@@ -0,0 +1,69 @@
+#include "gtest/gtest.h"
+
+extern "C" {
+#include "square_equation.h"
+}
+
+TEST(square_equation_two_solution, ok) {
+	double a = 1.0;
+	double b = -3.0;
+	double c = 2.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), 2);
+	ASSERT_EQ(Equal(x1, 1.0), 1);
+	ASSERT_EQ(Equal(x2, 2.0), 1);
+}
+
+TEST(square_equation_one_solution, ok) {
+	double a = 1.0;
+	double b = -4.0;
+	double c = 4.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), 1);
+	ASSERT_EQ(Equal(x1, 2.0), 1);
+}
+
+TEST(square_equation_no_solution, ok) {
+	double a = 1.0;
+	double b = 2.0;
+	double c = 5.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), 0);
+}
+
+TEST(linear_equation, ok) {
+	double a = 0.0;
+	double b = -3.0;
+	double c = 4.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), 1);
+	ASSERT_EQ(Equal(x1, -c / b), 1);
+}
+
+TEST(no_solutions, ok) {
+	double a = 0.0;
+	double b = 0.0;
+	double c = 1.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), 0);
+}
+
+TEST(infinity_solutions, ok) {
+	double a = 0.0;
+	double b = 0.0;
+	double c = 0.0;
+	double x1;
+	double x2;
+
+	ASSERT_EQ(SolveSquare(a, b, c, &x1, &x2), INF);
+}
